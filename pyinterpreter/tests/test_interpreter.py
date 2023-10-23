824ba3b44b50e3175a957ae95d0acd924021213a
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2023-10-23 21:57:18 +0800

    More tests
    Name                         Stmts   Miss  Cover
    ------------------------------------------------
    interpreter/__init__.py          1      0   100%
    interpreter/ast.py              21      3    86%
    interpreter/interpreter.py      38      3    92%
    interpreter/lexer.py            44      0   100%
    interpreter/parser.py           47      3    94%
    interpreter/token.py            13      1    92%
    tests/__init__.py                0      0   100%
    tests/test_interpreter.py       33      1    97%
    ------------------------------------------------
    TOTAL                          197     11    94%

--- pyinterpreter/tests/test_interpreter.py
+++ pyinterpreter/tests/test_interpreter.py
@@ -15,6 +15,24 @@ class TestInterpreter:
     def test_sub(self, interpreter):
         assert interpreter.eval("2-2") == 0
 
+    def test_mul(self, interpreter):
+        assert interpreter.eval("4*7") == 4*7
+
+    def test_div(self, interpreter):
+        assert interpreter.eval("4/7") == 4/7
+
+    def test_neg(self, interpreter):
+        assert interpreter.eval("---2") == ---2
+
+    def test_pos(self, interpreter):
+        assert interpreter.eval("+++2") == +++2
+
+    def test_neg_number(self, interpreter):
+        assert interpreter.eval("-2-2") == -2-2
+
+    def test_neg_expr(self, interpreter):
+        assert interpreter.eval("-(2*8)+100") == -(2*8)+100
+
     def test_add_with_letter(self, interpreter):
         with pytest.raises(Exception):
             interpreter.eval("2+a")
@@ -31,3 +49,4 @@ class TestInterpreter:
     )
     def test_add_spaces(self, interpreter, code):
         assert interpreter.eval(code) == 4
+
