159647b0f71995829625d387059ec70607991785
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2023-11-20 12:08:51 +0800

    Break out client handling into handle_client()

--- pseudopascal/server.py
+++ pseudopascal/server.py
@@ -2,6 +2,32 @@ from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 from interpreter import Interpreter
 import json
 
+def handle_client(client):
+	# Protocol:
+	# - Client sends program length
+	# - Client sends program code
+
+	while True:
+		msglen = int.from_bytes( client.recv(4) )
+
+		if msglen == 0:
+			break
+
+		program = client.recv(msglen).decode()
+
+		print("Program:", program)
+
+		interp = Interpreter()
+
+		try:
+			state = interp.eval(program)
+			response = json.dumps(state).encode()
+		except Exception as e:
+			response = json.dumps({"error": str(e)}).encode()
+
+		client.send(len(response).to_bytes(4))
+		client.send(response)
+
 def server(host, port):
 	with socket(AF_INET, SOCK_STREAM) as sock:
 		sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)	# Allow address reuse
@@ -15,32 +41,7 @@ def server(host, port):
 			try:
 				client, addr = sock.accept()
 				print("Client connected:", *addr)
-
-				# Protocol:
-				# - Client sends program length
-				# - Client sends program code
-
-				while True:
-					msglen = int.from_bytes( client.recv(4) )
-
-					if msglen == 0:
-						break
-
-					program = client.recv(msglen).decode()
-
-					print("Program:", program)
-
-					interp = Interpreter()
-
-					try:
-						state = interp.eval(program)
-						response = json.dumps(state).encode()
-					except Exception as e:
-						response = json.dumps({"error": str(e)}).encode()
-
-					client.send(len(response).to_bytes(4))
-					client.send(response)
-
+				handle_client(client)
 				print("Client", *addr, "disconnected")
 				client.close()
 			except BlockingIOError:
